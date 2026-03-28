aaa86e0fdf96701162a7530bba82d26a0a5437ff
Meringue-25 <yuki.li3465@gmail.com>
2026-03-28 14:22:11 -0700

    can add the task now but i dont understand why it has a task bar thing

--- garden.js
+++ garden.js
@@ -9,6 +9,16 @@ function addTask()  {
         return; 
     }
     const li = document.createElement("li");
+    li.innerHTML = `
+         <label>
+            <input type-"checkbox">
+            <span>${task}</span>
+        </label>
+        <span class="edit-btn">Edit</span>
+        <span class="delete-btn">Delete</span>
+    `;
+
     listContainer.appendChild(li);
+    inputBox.value = "";
 }
 
