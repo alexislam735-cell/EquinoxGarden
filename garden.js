43c2a60d2a726b2b411a87e3fee5c42e35793ce0
Meringue-25 <yuki.li3465@gmail.com>
2026-03-28 15:57:20 -0700

    fixed the js

--- garden.js
+++ garden.js
@@ -22,36 +22,34 @@ function addTask()  {
 
     listContainer.appendChild(li);
     inputBox.value = "";
+    const checkbox = li.querySelector("input");
+    const editBtn = li.querySelector(".edit-btn");
+    const taskSpan = li.querySelector("span");
+    const deleteBtn = li.querySelector(".delete-btn");
+    
+    const completedCounter = document.getElementById("completed-counter");
+    const uncompletedCounter = document.getElementById("uncompleted-counter");
+
+    function updateCounters() {
+        const completedTasks = document.querySelectorAll(".completed").length;
+        const uncompletedTasks =
+          document.querySelectorAll("li:not(.completed)").length;
+      
+        completedCounter.textContent = completedTasks;
+        uncompletedCounter.textContent = uncompletedTasks;
+      }
+    
+    checkbox.addEventListener("click", function () {
+        li.classList.toggle("completed", checkbox.checked);
+        updateCounters();
+      });
+    
+    editBtn.addEventListener("click", function () {
+        const update = prompt("Edit task:", taskSpan.textContent);
+        if (update !== null) {
+          taskSpan.textContent = update;
+          li.classList.remove("completed");
+        }
+    });
 }
-
-const checkbox = li.querySelector("input");
-const editBtn = li.querySelector(".edit-btn");
-const taskSpan = li.querySelector("span");
-const deleteBtn = li.querySelector(".delete-btn");
-
-const completedCounter = document.getElementById("completed-counter");
-const uncompletedCounter = document.getElementById("uncompleted-counter");
-
-function updateCounters() {
-    const completedTasks = document.querySelectorAll(".completed").length;
-    const uncompletedTasks =
-      document.querySelectorAll("li:not(.completed)").length;
-  
-    completedCounter.textContent = completedTasks;
-    uncompletedCounter.textContent = uncompletedTasks;
-  }
-
-checkbox.addEventListener("click", function () {
-    li.classList.toggle("completed", checkbox.checked);
-    updateCounters();
-  });
-
-editBtn.addEventListener("click", function () {
-    const update = prompt("Edit task:", taskSpan.textContent);
-    if (update !== null) {
-      taskSpan.textContent = update;
-      li.classList.remove("completed");
-    }
-});
-
 
