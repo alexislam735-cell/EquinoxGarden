f95ed07b8eac17f2808e8578e3353664172e2f65
Meringue-25 <yuki.li3465@gmail.com>
2026-03-28 15:06:00 -0700

    made an event listener for the button but gotta stop to fix the hr first

--- garden.js
+++ garden.js
@@ -24,3 +24,15 @@ function addTask()  {
     inputBox.value = "";
 }
 
+const checkbox = li.querySelector("input");
+const editBtn = li.querySelector(".edit-btn");
+const taskSpan = li.querySelector("span");
+const deleteBtn = li.querySelector(".delete-btn");
+
+checkbox.addEventListener("click", function () {
+    li.classList.toggle("completed", checkbox.checked);
+  });
+
+editBtn.addEventListener("click", function () {
+
+}
