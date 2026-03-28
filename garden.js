504f429b513748744320b762bddf6c3140a4a08e
Meringue-25 <yuki.li3465@gmail.com>
2026-03-28 13:33:46 -0700

    i forgot to put a source for the script

--- garden.js
+++ garden.js
@@ -1,13 +1,12 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+
 function addTask()  {
     const task = inputBox.value.trim();
     if (!task) {
         alert("Please write down a task");
-        return;
-        const li = document.createElement("li");
+        return; 
     }
-
-
 }
+const li = document.createElement("li");
